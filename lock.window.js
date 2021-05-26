71e711f9cf4301b77cfa68733811db0c36003cbe
Stanislav-Vyhnan <stas.vignan@gmail.com>
2021-05-26 19:30:12 +0300

    prettier.io

--- lock.window.js
+++ lock.window.js
@@ -1,27 +1,27 @@
-'use strict'
-const padawan = document.querySelector("#Padawan")
+"use strict";
+const padawan = document.querySelector("#Padawan");
 const jedi = document.querySelector("#Jedi");
-const checkYearsOld = document.querySelector(".check-years")
-const checkYearsItems = document.querySelector(".check-years-items")
+const checkYearsOld = document.querySelector(".check-years");
+const checkYearsItems = document.querySelector(".check-years-items");
 
 function checkYears(choice) {
-    document.body.style.position = 'fixed'
-    checkYearsOld.style.display = "flex"
+    document.body.style.position = "fixed";
+    checkYearsOld.style.display = "flex";
 }
-checkYears(0)
+checkYears(0);
 
 function unlock() {
-    document.body.style.position = 'static'
-    checkYearsOld.style.display = "none"
+    document.body.style.position = "static";
+    checkYearsOld.style.display = "none";
 }
 
 function lock() {
-    checkYearsItems.style.width = '300px'
-    checkYearsItems.style.height = '300px'
+    checkYearsItems.style.width = "300px";
+    checkYearsItems.style.height = "300px";
     checkYearsOld.innerHTML = `<div class="check-years-items">
     <h2>Sorry, You Are Not Allowed to Enter This Site.</h2>
     <img src="img/decorative-img.svg" alt="decorative-img">
-    <p>We don't sell alcohol to persons under 21 years old, according to federal laws.</p></div>`
+    <p>We don't sell alcohol to persons under 21 years old, according to federal laws.</p></div>`;
 }
-padawan.addEventListener('click', lock)
-jedi.addEventListener('click', unlock)
+padawan.addEventListener("click", lock);
+jedi.addEventListener("click", unlock);
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 let products;
 let elementId = 1;
 let startElementId = 1;
@@ -6,60 +6,60 @@ let startElementId = 1;
 function renderProducts(products) {
     for (const product of products) {
         let productsContainer = document.querySelector(`#slide-${elementId}`);
-        productsContainer.innerHTML = '';
+        productsContainer.innerHTML = "";
         productsContainer.innerHTML += `
             <img src="${product.imgUrl}" id="${product.id}" alt="${product.description}">
             <h5>${product.title}</h5>
             <p class="price">${product.price}</p>
             <div class="carousel-items-element-pin">Add to cart</div>`;
-        elementId >= 4 ? elementId = 1 : elementId <= 0 ? elementId = 1 : elementId++
+        elementId >= 4 ?
+            (elementId = 1) :
+            elementId <= 0 ?
+            (elementId = 1) :
+            elementId++;
     }
 }
 
-const carslBack = document.querySelector('.carousel-element-back');
-
+const carslBack = document.querySelector(".carousel-element-back");
 
 function nextSlide() {
     startElementId++;
     if (startElementId >= 5) startElementId = 1;
-    elementId = startElementId
+    elementId = startElementId;
     renderProducts(products);
 }
 
-const carslForward = document.querySelector('.carousel-element-forward');
-
+const carslForward = document.querySelector(".carousel-element-forward");
 
 function prevSlide() {
     startElementId--;
     if (startElementId <= 0) startElementId = 4;
-    elementId = startElementId
+    elementId = startElementId;
     renderProducts(products);
 }
 
 const cartPinOpen = document.querySelector(".carousel-items-element-pin");
-const cart = document.querySelector('.cart');
-let cartValue = document.querySelector('.cart-value');
+const cart = document.querySelector(".cart");
+let cartValue = document.querySelector(".cart-value");
 let i = 0;
 
 function openCart() {
-    i++
+    i++;
     cartValue.innerHTML = i;
     cart.style.display = "flex";
     if (i >= 10) {
         i = 9;
-        cartValue.innerHTML = i + '+';
+        cartValue.innerHTML = i + "+";
     }
-
 }
 
-
 async function fetchProducts() {
-    const response = await fetch('products.json');
+    const response = await fetch("products.json");
     products = await response.json();
     renderProducts(products);
 }
 fetchProducts();
 
-carslBack.addEventListener('click', prevSlide);
-carslForward.addEventListener('click', nextSlide);
-cartPinOpen.addEventListener('click', openCart)
+carslBack.addEventListener("click", prevSlide);
+carslForward.addEventListener("click", nextSlide);
+cartPinOpen.addEventListener("click", openCart);
